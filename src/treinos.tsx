08d65c95606a2cd3ec42f75023c38ba7f1db4c98
XaulimMatadordePorco244 <dmichaeldouglas00@gmail.com>
2025-07-02 11:09:45 -0400

    feat: cria estrutura inicial da página de treinos

---
+++ src/treinos.tsx
@@ -0,0 +1,9 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './treinos.css'
+
+function Students(){}
+
+export default Students
+
