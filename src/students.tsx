cf442f0b2c87738bdc5de8f1bb59a3e28cd3566c
XaulimMatadordePorco244 <dmichaeldouglas00@gmail.com>
2025-07-02 11:09:31 -0400

    feat: cria estrutura inicial da página de alunos

---
+++ src/students.tsx
@@ -0,0 +1,9 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './students.css'
+
+function Students(){}
+
+export default Students
+
